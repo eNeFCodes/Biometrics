735b1c870c53acaaf9d5744fd1be89e621e86406
Neil Francis Hipona <neil.francis.hipona@codeandtheory.com>
2022-02-28 10:58:37 +0800

    Initial Commit

---
+++ Biometrics/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Biometrics
+//
+//  Created by Neil Francis Hipona on 2/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
